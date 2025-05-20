5920b45b14187c5334cc1344feb5877b450f673c
Lucas Maion <lucasmgois@outlook.com>
2025-05-20 12:03:42 -0300

    feat: better error messaging

--- src/gateways/driver/api_rest/middlewares/error.middleware.ts
+++ src/gateways/driver/api_rest/middlewares/error.middleware.ts
@@ -4,24 +4,24 @@ import { ZodError } from "zod";
 
 export const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction): void => {
     if (error instanceof BadQueryError) {
-        res.status(400).send(error.message)
+        res.status(400).json({ message: error.message })
         return;
     }
     if (error instanceof BadRequestError) {
-        res.status(400).send(error.message)
+        res.status(400).json({ message: error.message })
         return;
     }
     if (error instanceof UserNotFoundError) {
-        res.status(404).send(error.message)
+        res.status(404).json({ message: error.message })
         return;
     }
     if (error instanceof WordNotFoundError) {
-        res.status(404).send(error.message)
+        res.status(404).json({ message: error.message })
         return;
     }
     if (error instanceof ZodError) {
-        res.status(400).send(error.issues.map(issue => issue.message).join(", "))
+        res.status(400).send({message: error.issues.map(issue => issue.message).join(", ")})
         return;
     }
-    res.status(500).send(error.message)
+    res.status(500).json({ message: "Erro interno no servidor" })
 };
